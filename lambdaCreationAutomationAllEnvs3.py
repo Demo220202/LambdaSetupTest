e59be42d6b0216d306231945f3c95febc4d39e36
z-aditya-pradhan <adityap@zenarate.com>
2025-12-20 01:02:33 +0530

    pipeline changes added

--- lambdaCreationAutomationAllEnvs3.py
+++ lambdaCreationAutomationAllEnvs3.py
@@ -79,7 +79,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--enable_reserved-concurrency",
+        "--enable_reserved_concurrency",
         action="store_true",
         help="Enable reserved concurrency"
     )
